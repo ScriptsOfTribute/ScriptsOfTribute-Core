38ed0d0bdca3761f61aed1c9e5f04a4dbcd395a0
Ematerasu <yungchochol@gmail.com>
2022-09-19 12:17:31 +0200

    Initial commit

---
+++ Card.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalesOfTribute
+{
+    public enum CardType
+    {
+        Action,
+        Agent,
+        ContractAction,
+        ContractAgent,
+        Starter,
+        Curse
+    }
+    public class Card
+    {
+        public string Name;
+        public int DeckID;
+        public int InstanceID;
+        public uint Cost;
+        public readonly CardType Type;
+        public int HP; // >=0 for Agent, -1 for other types
+        public bool Taunt;
+        public List<Effect> Effects;
+        public int Hash;
+        public int UpgradeCardID;
+        public int Copies;
+        public int CopiesUpgraded;
+
+        public Card()
+        {
+            Name = "null card";
+            Effects = new List<Effect>();
+        }
+
+        public Card(string name, int deckID, int instanceID, uint cost, CardType type, int hp, bool taunt, List<Effect> effects, int hash)
+        {
+            Name = name;
+            DeckID = deckID;
+            InstanceID = instanceID;
+            Cost = cost;
+            Type = type;
+            HP = hp;
+            Taunt = taunt;
+            Effects = effects;
+            Hash = hash;
+        }
+
+        public Card(string name, int deckID, int instanceID, uint cost, CardType type, int hp, bool taunt, List<Effect> effects, int hash, int upgradeCardID, int copies, int copiesUpgraded)
+        {
+            Name = name;
+            DeckID = deckID;
+            InstanceID = instanceID;
+            Cost = cost;
+            Type = type;
+            HP = hp;
+            Taunt = taunt;
+            Effects = effects;
+            Hash = hash;
+            UpgradeCardID = upgradeCardID;
+            Copies = copies;
+            CopiesUpgraded = copiesUpgraded;
+        }        
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# TalesOfTribute
+Impementing Tales of Tribute as a programming game
+
+
+## Coding Conventions
+https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
