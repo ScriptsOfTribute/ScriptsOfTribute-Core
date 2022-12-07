beb3f2af0ea737b578a418022868faa498b7debb
Ematerasu <yungchochol@gmail.com>
2022-12-07 21:43:18 +0100

    init commit for AI interface

---
+++ src/AI/AI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TalesOfTribute.src.AI
+{
+    public abstract class AI
+    {
+        public PlayerEnum PlayerID { get; set; }
+
+        public AI()
+        {
+        }
+
+        public abstract PatronId SelectPatron(List<PatronId> avalaiblePatrons); // Will be called only twice
+        public abstract Move Play(BoardSerializer board);
+        public abstract List<Effect> HandleEffectChoice(BoardSerializer board, Choice<Effect> choice);
+        public abstract List<Card> HandleCardChoice(BoardSerializer board, Choice<Card> choice);
+
+        public abstract
+    }
+}
---
+++ src/AI/Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TalesOfTribute.src.AI
+{
+    public class Game
+    {
+        private AI Player1;
+        private AI Player2;
+        private BoardManager board;
+        public Game(AI player1, AI player2)
+        {
+            Player1 = player1;
+            Player2 = player2;
+            Player1.PlayerID = PlayerEnum.PLAYER1;
+            Player2.PlayerID = PlayerEnum.PLAYER2;
+
+            board = new BoardManager(PatronSelection());
+        }
+
+        private PatronId[] PatronSelection()
+        {
+            List<PatronId> patrons = Enum.GetValues(typeof(PatronId)).Cast<PatronId>().ToList();
+            List<PatronId> patronsSelected = new List<PatronId>();
+            PatronId patron = Player1.SelectPatron(patrons);
+            patronsSelected.Add(patron);
+            patrons.Remove(patron);
+
+            patron = Player2.SelectPatron(patrons);
+            patronsSelected.Add(patron);
+            patrons.Remove(patron);
+
+            patronsSelected.Add(PatronId.TREASURY);
+
+            patron = Player2.SelectPatron(patrons);
+            patronsSelected.Add(patron);
+            patrons.Remove(patron);
+
+            patron = Player1.SelectPatron(patrons);
+            patronsSelected.Add(patron);
+            patrons.Remove(patron);
+
+            return patronsSelected.ToArray();
+        }
+
+        public void Play()
+        {
+
+        }
+    }
+}
