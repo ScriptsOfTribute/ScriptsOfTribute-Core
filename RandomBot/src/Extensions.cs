7c91b2fb3526acc4f16f1435201574fb8d951a53
Damian Kowalik <dev.damian.kowalik@gmail.com>
2022-12-15 14:28:53 +0100

    Move main project to separate folder and create basic bot

---
+++ RandomBot/src/Extensions.cs
@@ -0,0 +1,11 @@
+﻿namespace RandomBot;
+
+public static class Extensions
+{
+    private static readonly Random Rnd = new();
+
+    public static T PickRandom<T>(this List<T> source)
+    {
+        return source[Rnd.Next(source.Count)];
+    }
+}
