a045d67f1e4570a351517040530885f03c733e5c
Damian Kowalik <dev.damian.kowalik@gmail.com>
2022-12-28 00:01:09 +0100

    Add module tests

--- SimpleBotsTests/RandomGamesTests.cs
+++ SimpleBotsTests/RandomGamesTests.cs
@@ -100,27 +100,4 @@ public class RandomGamesTests
 
         _testOutputHelper.WriteLine(counter.ToString());
     }
-
-    [Fact]
-    void Test()
-    {
-        var br = new BoardManager(new[] { PatronId.PELIN, PatronId.RED_EAGLE, PatronId.ANSEI, PatronId.HLAALU });
-        var ritual = GlobalCardDatabase.Instance.GetCard(CardId.BRIARHEART_RITUAL);
-        br.Tavern.AvailableCards.Add(ritual);
-        var knightCommander = GlobalCardDatabase.Instance.GetCard(CardId.KNIGHT_COMMANDER);
-        br.CurrentPlayer.Hand.Add(knightCommander);
-        var knightsOfSaintPelin = GlobalCardDatabase.Instance.GetCard(CardId.KNIGHTS_OF_SAINT_PELIN);
-        br.CurrentPlayer.Hand.Add(knightsOfSaintPelin);
-        
-        br.PlayCard(knightsOfSaintPelin);
-
-        br.EndTurn();
-        br.EndTurn();
-
-        br.CurrentPlayer.CoinsAmount = 1000;
-        br.PlayCard(knightCommander);
-        br.BuyCard(ritual);
-        br.CardActionManager.MakeChoice(new List<Card> { knightCommander });
-        br.ActivateAgent(knightsOfSaintPelin);
-    }
 }
