765a7eb3727904e72699e48f59b0fad34081ed12
Damian Kowalik <dev.damian.kowalik@gmail.com>
2023-01-09 07:05:30 +0100

    Add initial ApplyState implementation for FairSerializedBoard that detects end turn

--- Engine/src/Serializers/FairSerializedBoard.cs
+++ Engine/src/Serializers/FairSerializedBoard.cs
@@ -19,26 +19,55 @@ public class FairSerializedBoard
     // Bot shouldn't have access to hand etc of enemy player.
     // Furthermore, when he simulates end turn (Move.EndTurn) he shouldn't have access to any information
     // about the other player and shouldn't be allowed to do basically anything.
+    // Therefore, in this view, CurrentPlayer should always refer to the bot and EnemyPlayer always to the enemy,
+    // even if it is his turn.
     public readonly FairSerializedPlayer CurrentPlayer;
     public readonly FairSerializedEnemyPlayer EnemyPlayer;
     
     // Completed actions are problematic, because they show what enemy player did on his turn, so may reveal
     // what cards he has.
+    // TODO: Figure out what to do and probably hide some.
     public readonly List<CompletedAction> CompletedActions;
 
     // Bot should know what cards are left in tavern, but not the exact order.
     private List<UniqueCard>? _tavernAvailableCards;
     public List<UniqueCard> TavernAvailableCards =>
         _tavernAvailableCards ??= _board.TavernAvailableCards.OrderBy(c => c.CommonId).ToList();
-    // Well, what if there are choices for DESTROY for enemy player? We shouldn't reveal that.
-    public SerializedChoice? PendingChoice => _board.PendingChoice;
+    // What if there are choices for DESTROY for enemy player? We shouldn't reveal that, so return null
+    // if turns changed.
+    public SerializedChoice? PendingChoice => _endOfTurnHappened ? null : _board.PendingChoice;
 
     private readonly SerializedBoard _board;
+    private readonly bool _endOfTurnHappened = false;
 
-    public FairSerializedBoard(SerializedBoard board)
+    public FairSerializedBoard(SerializedBoard board) : this(board, board.CurrentPlayer, board.EnemyPlayer)
+    {
+    }
+
+    public FairSerializedBoard(SerializedBoard board, SerializedPlayer currentPlayer, SerializedPlayer enemyPlayer, bool endOfTurnHappened = false)
     {
         _board = board;
         CurrentPlayer = new FairSerializedPlayer(board.CurrentPlayer);
         EnemyPlayer = new FairSerializedEnemyPlayer(board.EnemyPlayer);
+        _endOfTurnHappened = endOfTurnHappened;
+    }
+
+    public (FairSerializedBoard, List<Move>) ApplyState(Move move)
+    {
+        if (_endOfTurnHappened)
+        {
+            throw new Exception("You can't simulate any more moves as you've ended your turn.");
+        }
+        // This means bot wants to see what happens after end of this turn.
+        // This means CurrentPlayer would be the bots' enemy, so he should have severely limited visibility.
+        var (newBoard, newMoves) = _board.ApplyState(move);
+
+        if (move.Command == CommandEnum.END_TURN)
+        {
+            return (new FairSerializedBoard(newBoard, _board.EnemyPlayer, _board.CurrentPlayer, true),
+                new List<Move>());
+        }
+
+        return (new FairSerializedBoard(newBoard), newMoves);
     }
 }
