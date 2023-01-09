5a84c2467a3dcb086f360f144d80c2c20a8705c2
Damian Kowalik <dev.damian.kowalik@gmail.com>
2023-01-09 06:57:07 +0100

    Add basic structure for fair SerializedBoard that doesn't reveal too much info

---
+++ Engine/src/Serializers/FairSerializedBoard.cs
@@ -0,0 +1,44 @@
+﻿using TalesOfTribute.Board;
+using TalesOfTribute.Board.CardAction;
+using TalesOfTribute.Board.Cards;
+
+namespace TalesOfTribute.Serializers;
+
+public class FairSerializedBoard
+{
+    public PatronStates PatronStates => _board.PatronStates;
+    public List<PatronId> Patrons => PatronStates.All.Select(p => p.Key).ToList();
+    public List<UniqueCard> TavernCards => _board.TavernCards;
+    public BoardState BoardState => _board.BoardState;
+    public ComboStates ComboStates => _board.ComboStates;
+    public ulong Seed => _board.Seed;
+    public List<UniqueBaseEffect> UpcomingEffects => _board.UpcomingEffects;
+    public List<UniqueBaseEffect> StartOfNextTurnEffects => _board.StartOfNextTurnEffects;
+    public EndGameState? GameEndState => _board.GameEndState;
+
+    // Bot shouldn't have access to hand etc of enemy player.
+    // Furthermore, when he simulates end turn (Move.EndTurn) he shouldn't have access to any information
+    // about the other player and shouldn't be allowed to do basically anything.
+    public readonly FairSerializedPlayer CurrentPlayer;
+    public readonly FairSerializedEnemyPlayer EnemyPlayer;
+    
+    // Completed actions are problematic, because they show what enemy player did on his turn, so may reveal
+    // what cards he has.
+    public readonly List<CompletedAction> CompletedActions;
+
+    // Bot should know what cards are left in tavern, but not the exact order.
+    private List<UniqueCard>? _tavernAvailableCards;
+    public List<UniqueCard> TavernAvailableCards =>
+        _tavernAvailableCards ??= _board.TavernAvailableCards.OrderBy(c => c.CommonId).ToList();
+    // Well, what if there are choices for DESTROY for enemy player? We shouldn't reveal that.
+    public SerializedChoice? PendingChoice => _board.PendingChoice;
+
+    private readonly SerializedBoard _board;
+
+    public FairSerializedBoard(SerializedBoard board)
+    {
+        _board = board;
+        CurrentPlayer = new FairSerializedPlayer(board.CurrentPlayer);
+        EnemyPlayer = new FairSerializedEnemyPlayer(board.EnemyPlayer);
+    }
+}
---
+++ Engine/src/Serializers/FairSerializedPlayer.cs
@@ -0,0 +1,49 @@
+﻿using TalesOfTribute.Board.Cards;
+
+namespace TalesOfTribute.Serializers;
+
+public class FairSerializedEnemyPlayer
+{
+    public PlayerEnum PlayerID => _player.PlayerID;
+    public List<SerializedAgent> Agents => _player.Agents;
+    public int Power => _player.Power;
+    public uint PatronCalls => _player.PatronCalls;
+    public int Coins => _player.Coins;
+    public int Prestige => _player.Prestige;
+
+    // TODO: Should we be able to see enemy's Played and Cooldown piles? What about draw?
+    public List<UniqueCard> Played => _player.Played;
+    public List<UniqueCard> CooldownPile => _player.CooldownPile;
+
+    private readonly SerializedPlayer _player;
+
+    public FairSerializedEnemyPlayer(SerializedPlayer player)
+    {
+        _player = player;
+    }
+}
+
+public class FairSerializedPlayer
+{
+    public PlayerEnum PlayerID => _player.PlayerID;
+    public List<UniqueCard> Hand => _player.Hand;
+    // Cooldown Pile can be returned as-is, because it gets reordered anyway.
+    public List<UniqueCard> CooldownPile => _player.CooldownPile;
+    public List<UniqueCard> Played => _player.Played;
+    public List<UniqueCard> KnownUpcomingDraws => _player.KnownUpcomingDraws;
+    public List<SerializedAgent> Agents => _player.Agents;
+    public int Power => _player.Power;
+    public uint PatronCalls => _player.PatronCalls;
+    public int Coins => _player.Coins;
+    public int Prestige => _player.Prestige;
+
+    private List<UniqueCard>? _drawPile;
+    public List<UniqueCard> DrawPile => _drawPile ??= _player.DrawPile.OrderBy(c => c.CommonId).ToList();
+
+    private readonly SerializedPlayer _player;
+
+    public FairSerializedPlayer(SerializedPlayer player)
+    {
+        _player = player;
+    }
+}
